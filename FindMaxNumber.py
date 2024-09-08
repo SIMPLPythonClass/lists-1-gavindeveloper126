e00b15a34fe6c468eeb96b3e2982085b81608795
gavindeveloper126 <gavinpeck5@gmail.com>
2024-09-08 21:11:23 +0000

    tried it

--- FindMaxNumber.py
+++ FindMaxNumber.py
@@ -4,10 +4,17 @@
 """
 
 
-def main():
+def main(high):
     numbers = [23, 12, 56, 78, 34, 67]
-    # WRITE HERE
-    pass
+    high = numbers[0]
+    for i in range (0,(len(numbers)-1)):
+        if high > numbers[i]:
+            high = high
+        elif high < numbers[i]:
+            high = numbers [i]
+        
+        return high
 
+print(high)
 if __name__ == '__main__':
     main()
